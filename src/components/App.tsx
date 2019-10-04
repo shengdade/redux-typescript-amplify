9a20d429827d0eb454667ffc6d5fdf0e527faf1c
Dade Sheng <dasheng@amazon.com>
2019-10-04 00:09:16 -0400

    Raw UI for creating post and listing posts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../store';
+import { PostState } from '../store/post/types';
+import { createPost } from '../store/post/actions';
 
-const App = () => {
-  return <div>Hello World</div>;
+interface AppProps {
+  post: PostState;
+  createPost: typeof createPost;
+}
+
+const App: React.FC<AppProps> = ({ post, createPost }) => {
+  const [title, setTitle] = useState('');
+
+  const submit = () => {
+    if (title) {
+      createPost({
+        title,
+        timestamp: new Date().getTime()
+      });
+      setTitle('');
+    }
+  };
+
+  const keyPress = (e: React.KeyboardEvent<any>) => {
+    if (e.key === 'Enter') {
+      submit();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value);
+        }}
+        onKeyPress={keyPress}
+        placeholder="Post something..."
+      />
+      <button onClick={submit}>Post</button>
+      {post.posts.map(post => (
+        <div key={post.timestamp}>
+          <h3>{post.title}</h3>
+          <p>{new Date(post.timestamp).toLocaleString()}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default App;
+const mapStateToProps = (state: AppState) => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { createPost }
+)(App);
